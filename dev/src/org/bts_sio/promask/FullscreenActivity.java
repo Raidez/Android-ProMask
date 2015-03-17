f37641d9768b33343714f13b95d65ade6bb84b1d
Raidez <alexis.lucien@bts-malraux.net>
2015-03-17 17:24:39 +0100

    - amélioration de "l'interface graphique"
    TODO :
    - limiter les déplacements selon la taille de l'écran
    - ajouter un menu permettant de configurer l'adresse IP et le port

--- dev/src/org/bts_sio/promask/FullscreenActivity.java
+++ dev/src/org/bts_sio/promask/FullscreenActivity.java
@@ -4,24 +4,27 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Collection;
 import java.util.Vector;
 
-import org.apache.http.conn.util.InetAddressUtils;
 import org.bts_sio.promask.util.SystemUiHider;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.*;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.*;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 import com.illposed.osc.*;
 
-public class FullscreenActivity extends Activity implements OnClickListener, OnSeekBarChangeListener {
-	private SystemUiHider ui;
+public class FullscreenActivity extends Activity implements OnClickListener, OnTouchListener {
+	//private SystemUiHider ui;
+	private int mActionBarSize;
 	private View screen;
-	private SeekBar[] writes;
+	private ImageButton[] writes;
 	public static int SEEKBAR_STEP = 10;
 
 	@Override
@@ -32,80 +35,33 @@ public class FullscreenActivity extends Activity implements OnClickListener, OnS
 		//#part : configuration de l'écran
 		screen = this.findViewById(R.id.fullscreen_content);
 		screen.setOnClickListener(this);
-		ui = SystemUiHider.getInstance(this, screen, SystemUiHider.FLAG_HIDE_NAVIGATION);
-		ui.setup();
+		//ui = SystemUiHider.getInstance(this, screen, SystemUiHider.FLAG_HIDE_NAVIGATION);
+		//ui.setup();
+		final TypedArray styledAttributes = getBaseContext().getTheme().obtainStyledAttributes(
+				new int[] { android.R.attr.actionBarSize });
+		mActionBarSize = (int) styledAttributes.getDimension(0, 0);
+		styledAttributes.recycle();
 		
-		//#part : configuration des paquets OSC
-		/*
-		Thread thread = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				OSCMessage msg = new OSCMessage("/mallarme/masque/test");
-
-				try {
-					OSCPortOut sender = new OSCPortOut(InetAddress.getByName("172.16.100.176"), 12345);
-					sender.send(msg);
-				} catch (SocketException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (UnknownHostException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-		thread.start();
-		*/
-		Vector test = new Vector(2);
-		test.add(0.5);
-		test.add(0.5);
-		
-		OSCMessage msg = new OSCMessage("/mallarme/masque/A", test);
-		this.OSCSend("172.16.100.176", 12345, msg);
-		
-		//#part : configuration des seekbars
-		writes = new SeekBar[4];
-		writes[0] = (SeekBar) this.findViewById(R.id.writeA);
-		writes[1] = (SeekBar) this.findViewById(R.id.writeB);
-		writes[2] = (SeekBar) this.findViewById(R.id.writeC);
-		writes[3] = (SeekBar) this.findViewById(R.id.writeD);
+		//#part : configuration du test
+		writes = new ImageButton[4];
+		writes[0] = (ImageButton)this.findViewById(R.id.A);
+		writes[1] = (ImageButton)this.findViewById(R.id.B);
+		writes[2] = (ImageButton)this.findViewById(R.id.C);
+		writes[3] = (ImageButton)this.findViewById(R.id.D);
 		
 		for(int i = 0; i < writes.length; i++)
-		{
-			writes[i].setMax(SEEKBAR_STEP);
-			writes[i].setOnSeekBarChangeListener(this);
-		}
+			writes[i].setOnTouchListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		if(v.getId() == R.id.fullscreen_content) {
-			ui.toggle();
+			//ui.toggle();
 		}
 	}
-
-	@Override
-	public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onStartTrackingTouch(SeekBar seekBar) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onStopTrackingTouch(SeekBar seekBar) {
-		double percentage = (double)seekBar.getProgress() / (double)SEEKBAR_STEP;
-		Log.d("pourcentage de la selection", "" + percentage);
-	}
-	
+	/*
+		OSCMessage msg = new OSCMessage("/mallarme/masque/A", data);
+		this.OSCSend("172.16.100.176", 12345, msg);*/
 	public void OSCSend(final String addr, final int port, final OSCMessage msg) {
 		Thread temp = new Thread(new Runnable() {
 			@Override
@@ -127,5 +83,25 @@ public class FullscreenActivity extends Activity implements OnClickListener, OnS
 		});
 		
 		temp.start();
+	}
+	
+	public int limitScreen(int size, float position) {
+		int new_position = (int)position - size / 2;
+		return new_position;
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		v.offsetLeftAndRight(limitScreen(v.getWidth(), event.getX()));
+		v.offsetTopAndBottom(limitScreen(v.getHeight(), event.getY()));
+		
+		float X = (float)v.getLeft() / (float)screen.getWidth() + (float)v.getWidth() / 2 / (float)screen.getWidth();
+		float Y = (float)v.getTop() / ((float)screen.getHeight() + mActionBarSize) + (float)v.getHeight() / 2 / ((float)screen.getHeight() + mActionBarSize);
+		Vector send = new Vector(2);
+		send.add(X);
+		send.add(Y);
+		OSCMessage msg = new OSCMessage("/mallarme/masque/"+v.getResources().getResourceEntryName(v.getId()), send);
+		OSCSend("172.16.100.176", 12345, msg);
+		return false;
 	}
 }
