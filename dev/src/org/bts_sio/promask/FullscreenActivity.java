661620a4d6a7699f513474d9d25811f7e87e9a38
Raidez <alexis.lucien@bts-malraux.net>
2015-03-24 16:13:41 +0100

    - limitation des déplacements selon la taille de l'écran
    TODO :
    - ajouter un menu pour configurer le port et l'adresse IP

--- dev/src/org/bts_sio/promask/FullscreenActivity.java
+++ dev/src/org/bts_sio/promask/FullscreenActivity.java
@@ -1,48 +1,35 @@
 package org.bts_sio.promask;
 
+import com.illposed.osc.*;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.Collection;
 import java.util.Vector;
 
-import org.bts_sio.promask.util.SystemUiHider;
-
-import android.app.ActionBar;
 import android.app.Activity;
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.*;
-import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.*;
-import com.illposed.osc.*;
 
-public class FullscreenActivity extends Activity implements OnClickListener, OnTouchListener {
-	//private SystemUiHider ui;
-	private int mActionBarSize;
+public class FullscreenActivity extends Activity implements OnTouchListener {
 	private View screen;
 	private ImageButton[] writes;
-	public static int SEEKBAR_STEP = 10;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState); //#detail : création/reprise de l'activité
+		//#part : configuration de l'écran
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE); //#detail : configure l'écran sans l'actionBar
 		this.setContentView(R.layout.activity_fullscreen); //#detail : application du xml à l'écran
 		
 		//#part : configuration de l'écran
 		screen = this.findViewById(R.id.fullscreen_content);
-		screen.setOnClickListener(this);
-		//ui = SystemUiHider.getInstance(this, screen, SystemUiHider.FLAG_HIDE_NAVIGATION);
-		//ui.setup();
-		final TypedArray styledAttributes = getBaseContext().getTheme().obtainStyledAttributes(
-				new int[] { android.R.attr.actionBarSize });
-		mActionBarSize = (int) styledAttributes.getDimension(0, 0);
-		styledAttributes.recycle();
 		
-		//#part : configuration du test
+		//#part : récupération/configuration des bouttons
 		writes = new ImageButton[4];
 		writes[0] = (ImageButton)this.findViewById(R.id.A);
 		writes[1] = (ImageButton)this.findViewById(R.id.B);
@@ -52,16 +39,7 @@ public class FullscreenActivity extends Activity implements OnClickListener, OnT
 		for(int i = 0; i < writes.length; i++)
 			writes[i].setOnTouchListener(this);
 	}
-
-	@Override
-	public void onClick(View v) {
-		if(v.getId() == R.id.fullscreen_content) {
-			//ui.toggle();
-		}
-	}
-	/*
-		OSCMessage msg = new OSCMessage("/mallarme/masque/A", data);
-		this.OSCSend("172.16.100.176", 12345, msg);*/
+	
 	public void OSCSend(final String addr, final int port, final OSCMessage msg) {
 		Thread temp = new Thread(new Runnable() {
 			@Override
@@ -91,17 +69,35 @@ public class FullscreenActivity extends Activity implements OnClickListener, OnT
 	}
 
 	@Override
-	public boolean onTouch(View v, MotionEvent event) {
+	public boolean onTouch(View obj, MotionEvent event) {
+		/*
 		v.offsetLeftAndRight(limitScreen(v.getWidth(), event.getX()));
 		v.offsetTopAndBottom(limitScreen(v.getHeight(), event.getY()));
+		Log.d("taille du layout", screen.getHeight()+" x "+screen.getWidth());
 		
-		float X = (float)v.getLeft() / (float)screen.getWidth() + (float)v.getWidth() / 2 / (float)screen.getWidth();
-		float Y = (float)v.getTop() / ((float)screen.getHeight() + mActionBarSize) + (float)v.getHeight() / 2 / ((float)screen.getHeight() + mActionBarSize);
+		
+		return false;*/
+		//Log.d("screenY", ""+screen.getHeight());
+		Log.d("action", ""+event.getAction());
+		if(event.getAction() ==  MotionEvent.ACTION_UP)
+		{
+			if((obj.getTop()+obj.getHeight()) >= screen.getHeight())
+				obj.offsetTopAndBottom(- obj.getHeight()*2);
+		}
+		else
+			obj.offsetTopAndBottom(limitScreen(obj.getHeight(), event.getY()));
+		
+		obj.offsetLeftAndRight(limitScreen(obj.getWidth(), event.getX()));
+		
+		
+		float X = (float)obj.getLeft() / (float)screen.getWidth() + (float)obj.getWidth() / 2 / (float)screen.getWidth();
+		float Y = (float)obj.getTop() / (float)screen.getHeight() + (float)obj.getHeight() / 2 / (float)screen.getHeight();
 		Vector send = new Vector(2);
 		send.add(X);
 		send.add(Y);
-		OSCMessage msg = new OSCMessage("/mallarme/masque/"+v.getResources().getResourceEntryName(v.getId()), send);
+		OSCMessage msg = new OSCMessage("/mallarme/masque/"+obj.getResources().getResourceEntryName(obj.getId()), send);
 		OSCSend("172.16.100.176", 12345, msg);
+		
 		return false;
 	}
 }
