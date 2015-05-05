cd452680f798160e54405aaf68773b379f74cb72
Alexis Lucien <alexis.lucien@bts-malraux.net>
2015-05-05 16:25:48 +0200

    - documentation fait avec mkdocs

---
+++ docs/mkdocs/docs/vanilla.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+	//#part: suppression du menu de base
+	var body = document.getElementsByTagName('body')[0];
+	body.className = "";
+
+	var section = document.getElementsByTagName('section')[0];
+	section.className = "";
+};
